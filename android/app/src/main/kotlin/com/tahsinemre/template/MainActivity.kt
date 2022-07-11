ae17682f788a70a022e779d84f006f4d3138c578
Tahsin Emre <telli@tahsinemre.com>
2022-07-11 21:15:23 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/tahsinemre/template/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tahsinemre.template
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //initialize firebase
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(),
+      debugShowCheckedModeBanner: false,
+      title: 'Template App',
+      home: Container(),
+    );
+  }
+}
